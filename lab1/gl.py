40fd8b54f41e390be3ae0ebf9a8bfec58a933612
JoseOrdonezB <ord231329@uvg.edu.gt>
2025-07-09 18:44:39 -0600

    llenado de poligonos

--- lab1/gl.py
+++ lab1/gl.py
@@ -7,6 +7,7 @@ class Renderer:
 
         self.glClear()
 
+    # se define el color de fondo.
     def glClearColor(self, r, g, b):
         r = min(1, max(0, r))
         g = min(1, max(0, g))
@@ -14,6 +15,7 @@ class Renderer:
 
         self.clearColor = [r, g, b]
 
+    # función para definir el color actual.
     def glColor(self, r, g, b): 
         r = min(1, max(0, r))
         g = min(1, max(0, g))
@@ -21,6 +23,7 @@ class Renderer:
 
         self.currColor = [r, g, b]
 
+    # se llena el frame buffer con el color de fondo.
     def glClear(self):
         color = [int(i*255) for i in self.clearColor]
         self.screen.fill(color)
@@ -28,6 +31,7 @@ class Renderer:
         self.frameBuffer = [[self.clearColor for i in range(self.height)]
                          for x in range(self.width)] 
 
+    # función para dibujar un punto en pantalla.
     def glPoint(self, x, y, color = None):
         x = round(x)
         y = round(y)
@@ -38,6 +42,7 @@ class Renderer:
 
             self.frameBuffer[x][y] = color
 
+    # función para dibujar líneas con el algoritmo de Bresenham.
     def glLine(self, p0, p1, color = None):
         x0 = p0[0]
         x1 = p1[0]
@@ -85,10 +90,68 @@ class Renderer:
                 
                 limit += 1
 
-    # función para dibujar polígonos
+    # función para dibujar polígonos.
     def glPolygon(self, points, color = None):
+        # se define la cantidad de puentos que hay en el polígono.
         n = len(points)
+
+        # si hay menos de tres puntos, solo se dibuja una línea.
+        if n < 3:
+            self.glLine(points[0], points[1], color or self.currColor)
+
+        # se recorre una lista de puntos y se dibujan las líneas para formar el polígono.
+        # se usa el operador módulo para conectar el último punto con el primero.
         for i in range(n):
             p0 = points[i]
             p1 = points[(i + 1) % n]
             self.glLine(p0, p1, color or self.currColor)
+
+    # función para rellenar polígonos.
+    # algoritmo de scanline filling.
+    # pequeña explicación: se dibuja una linea horizontal en cada valor de y del polígono, donde se intersecta con los bordes se dibujan puntos para llenar la figura.
+    def glFillPolygon(self, points, color = None):
+        color = color or self.currColor
+
+        # se define la cantidad de puntos que hay en el polígono.
+        n = len(points)
+
+        # si hay menos de tres puntos no se rellena nada ya que no hay polígono.
+        if n < 3:
+            return
+        
+        # se obtienen los valores mínimos y máximos de y.
+        y_min = min(p[1] for p in points)
+        y_max = max(p[1] for p in points)
+        
+        # se reccore un ciclo desde el minimo de y hasta el máximo de y.
+        for y in range(y_min, y_max + 1):
+            # se crea una lista para guardar los valores en los que se intersecta la línea horizontal.
+            intersections = []
+
+            # se recorre cada punto del polígono y se calcula la intersección con la línea horizontal.
+            for i in range(n):
+                (x0, y0) = points[i]
+                (x1, y1) = points[(i + 1) % n]
+
+                # si los puntos son iguales, no se hace nada.
+                if y0 == y1:
+                    continue
+
+                # si la línea horizontal intersecta con el borde del polígono, se calcula el valor de x.
+                if (y >= min(y0, y1) and (y < max(y0, y1))):
+                    x = int(x0 + (y - y0) * (x1 - x0) / (y1 - y0))
+                    intersections.append(x)
+
+            intersections.sort()
+
+            # se recorre la lista de las intersecciones.
+            for i in range(0, len(intersections), 2):
+                # mientras hayan pares de intersecciones, se dibujan puntos entre la x inicial y x final del par.
+                if i + 1 < len(intersections):
+                    x_start = intersections[i]
+                    x_end = intersections[i + 1]
+                    for x in range(x_start, x_end + 1):
+                        self.glPoint(x, y, color)
+
+
+
--- lab1/rasterizacion.py
+++ lab1/rasterizacion.py
@@ -21,26 +21,27 @@ while isRunning:
     poly1 = [(165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)]
     rend.glClear()
     rend.glPolygon(poly1)
+    rend.glFillPolygon(poly1)
 
     # poligono 2
     poly2 = [(321, 335), (288, 286), (339, 251), (374, 302)]
-    
     rend.glPolygon(poly2)
+    rend.glFillPolygon(poly2)
 
     # poligono 3
     poly3 = [(377, 249), (411, 197), (436, 249)]
-    
     rend.glPolygon(poly3)
+    rend.glFillPolygon(poly3)
 
     # poligono 4
     poly4 = [(413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37), (660, 52), (750, 145), (761, 179), (672, 192), (659, 214), (615, 214), (632, 230), (580, 230), (597, 215), (552, 214), (517, 144), (466, 180)]
-    
     rend.glPolygon(poly4)
+    rend.glFillPolygon(poly4)
 
     # poligono 5
     poly5 = [(682, 175), (708, 120), (735, 148), (739, 170)]
-    
     rend.glPolygon(poly5)
+    rend.glFillPolygon(poly5, color=(0, 0, 0))
 
 
     pygame.display.flip()
